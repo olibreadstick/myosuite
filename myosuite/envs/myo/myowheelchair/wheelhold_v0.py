2cfcc7c163616d7fd91762268be08c88a30e8869
olibreadstick <122502760+olibreadstick@users.noreply.github.com>
2025-07-14 14:00:06 -0400

    Changes with Cheryl

--- myosuite/envs/myo/myowheelchair/wheelhold_v0.py
+++ myosuite/envs/myo/myowheelchair/wheelhold_v0.py
@@ -12,10 +12,10 @@ from myosuite.envs.myo.base_v0 import BaseV0
 
 class WheelHoldFixedEnvV0(BaseV0):
 
-    DEFAULT_OBS_KEYS = ['wheel_err_right', 'hand_qpos', 'hand_qvel']
+    DEFAULT_OBS_KEYS = ['time', 'wheel_err_right', 'hand_qpos', 'hand_qvel']
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
         "goal_dist": 100.0,
-        "bonus": 4.0,
+        "bonus": 0.0,
         "penalty": 10,
     }
 
@@ -60,8 +60,8 @@ class WheelHoldFixedEnvV0(BaseV0):
 
     def get_obs_vec(self):
         self.obs_dict['time'] = np.array([self.sim.data.time])
-        self.obs_dict['hand_qpos'] = self.sim.data.qpos[7:].copy()
-        self.obs_dict['hand_qvel'] = self.sim.data.qvel[6:].copy()*self.dt
+        self.obs_dict['hand_qpos'] = self.sim.data.qpos[13:].copy()
+        self.obs_dict['hand_qvel'] = self.sim.data.qvel[12:].copy()*self.dt
         #self.obs_dict['wheel_pos'] = self.sim.data.site_xpos[self.object_sid]
         self.obs_dict['wheel_err_right'] = self.sim.data.site_xpos[self.goal_sid_right] - self.sim.data.site_xpos[self.palm_r]
         #self.obs_dict['wheel_err_left'] = self.sim.data.site_xpos[self.goal_sid] - self.sim.data.site_xpos[self.object_sid]
@@ -76,10 +76,12 @@ class WheelHoldFixedEnvV0(BaseV0):
         obs_dict = {}
         obs_dict['time'] = np.array([sim.data.time])
         obs_dict['hand_qpos'] = sim.data.qpos[13:].copy()
-        obs_dict['hand_qvel'] = sim.data.qvel[14:].copy()*self.dt
+        obs_dict['hand_qvel'] = sim.data.qvel[12:].copy()*self.dt
         #obs_dict['wheel_pos'] = sim.data.site_xpos[self.object_sid]
         #obs_dict['wheelchair_grip_right'] = sim.data.site_xpos[self.goal_sid] - sim.data.site_xpos[self.object_sid]
         obs_dict['wheel_err_right'] = sim.data.site_xpos[self.goal_sid_right] - sim.data.site_xpos[self.palm_r]
+        #add the initial and end target points
+        #could add the fingertips here,
         #obs_dict['wheel_err_left'] = sim.data.site_xpos[self.goal_sid] - sim.data.site_xpos[self.object_sid]
         if sim.model.na>0:
             obs_dict['act'] = sim.data.act[:].copy()
@@ -89,7 +91,7 @@ class WheelHoldFixedEnvV0(BaseV0):
     def get_reward_dict(self, obs_dict):
         dist_right = np.linalg.norm(obs_dict['wheel_err_right'])
         act_mag = np.linalg.norm(self.obs_dict['act'], axis=-1)/self.sim.model.na if self.sim.model.na !=0 else 0
-
+        drop = dist_right > 0.300
         
         # grip_right = self._check_hand_grip_contact(
         #     hand_geom_names=["right_index_tip", "right_thumb_tip"],
@@ -97,21 +99,26 @@ class WheelHoldFixedEnvV0(BaseV0):
         # )
 
         rwd_dict = collections.OrderedDict((
-            ('goal_dist', -dist_right),
+            ('goal_dist', -dist_right), #exp(- k * abs(x))
+            ('bonus', 1.*(dist_right<2*0) + 1.*(dist_right<0)),
             ('act_reg', -1.*act_mag),
             #('grip_bonus', 1.0 * grip_right),
-            ('sparse', 1.0 * -dist_right),
+            ('penalty', -1.*drop),
+            ('sparse', dist_right < 0.055),
             #('sparse', 1.0 * grip_right - dist_right),
             ('solved', dist_right < 0.015),
             #('solved', grip_right and dist_right < 0.015),
-            ('done', dist_right < 0.001),
+            ('done', dist_right > 0.515),
         ))
         
-        rwd_dict['dense'] = 1.0 * rwd_dict['goal_dist']
-    
-        #include below when grip is added
-        #rwd_dict['dense'] = 5.0 * rwd_dict['grip_bonus'] + 1.0 * rwd_dict['goal_dist']
+        rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
+        
         return rwd_dict
+    
+    def reset(self, **kwargs):
+        self.robot.sync_sims(self.sim, self.sim_obsd)
+        obs = super().reset(**kwargs)
+        return obs
 
     # def _check_hand_grip_contact(self, hand_geom_names, wheel_geom_names):
     #     hand_geom_ids = [self.sim.model.geom_name2id(n) for n in hand_geom_names]
@@ -128,13 +135,3 @@ class WheelHoldFixedEnvV0(BaseV0):
 
 # class WheelHoldRandomEnvV0(WheelHoldFixedEnvV0):
 
-#     def reset(self, **kwargs):
-#         # randomize target pos
-#         self.sim.model.site_pos[self.goal_sid] = self.object_init_pos + self.np_random.uniform(high=np.array([0.030, 0.030, 0.030]), low=np.array([-.030, -.030, -.030]))
-#         # randomize object
-#         size = self.np_random.uniform(high=np.array([0.030, 0.030, 0.030]), low=np.array([.020, .020, .020]))
-#         self.sim.model.geom_size[-1] = size
-#         self.sim.model.site_size[self.goal_sid] = size
-#         self.robot.sync_sims(self.sim, self.sim_obsd)
-#         obs = super().reset(**kwargs)
-#         return obs
