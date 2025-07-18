4dcc4a3ab2008bedc7ee2a0b339c742c8d72eef6
olibreadstick <122502760+olibreadstick@users.noreply.github.com>
2025-07-18 16:53:45 -0400

    Wheel rotation policy added, not working, and left arm added not finished

--- myosuite/envs/myo/myowheelchair/ParallelTrainingScriptWandB.py
+++ myosuite/envs/myo/myowheelchair/ParallelTrainingScriptWandB.py
@@ -41,7 +41,8 @@ curr_dir = os.path.dirname(os.path.abspath(__file__))
 def record_video(env_name, model_path=curr_dir+'/WheelDist_policy', out_path=curr_dir+"/videos/Wheel_MinDist.mp4"):
     env = gym.make(env_name)
     env.reset()
-    model = PPO.load(model_path)
+    #model = PPO.load(model_path)
+    model = PPO("MlpPolicy", envs, verbose=1, ent_coef=0.01, policy_kwargs=policy_kwargs)
     frames = []
     for _ in range(300):
         frames.append(env.sim.renderer.render_offscreen(width=400, height=400, camera_id=0))
@@ -166,13 +167,12 @@ if __name__ == "__main__":
     # model = PPO('MlpPolicy', envs, verbose=1, ent_coef=0.001, policy_kwargs=policy_kwargs)
 
     model_num =   '2025_07_16_13_12_43' # Loaded WheelDist_policy_GOOD
-    model = PPO.load('./MPL_baselines/policy_best_model'+ '/'+ env_name + '/' + model_num + r'/best_model', envs, verbose = 1, ent_coeff = 0.01, policy_kwargs = policy_kwargs)
-
+    model = PPO("MlpPolicy", envs, verbose=1, ent_coef=0.01, policy_kwargs=policy_kwargs)
     obs_callback = TensorboardCallback()
     callback = CallbackList([eval_callback, WandbCallback(gradient_save_freq=100)])#, obs_callback])
 
     #TODO TOTAL TIMESTEPS HERE
-    model.learn(total_timesteps=1000, tb_log_name=env_name + "_" + time_now, callback=callback)
+    model.learn(total_timesteps=5e5, tb_log_name=env_name + "_" + time_now, callback=callback)
     model.save(curr_dir+'/WheelDist_policy')
 
     # Record video after training
